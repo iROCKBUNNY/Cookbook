51de430ad0f30c53ab9145b735c361191da157ec
iROCKBUNNY <irockbunny@outlook.com>
2018-03-24 19:01:07 +0800

    Update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,8 @@
 // common.js
 
 // updated at
-moment.locale('zh-cn');
 function updateVersion(timestamp) {
-    $('#updated-at img').attr('src', 'https://img.shields.io/badge/%E6%9B%B4%E6%96%B0%E4%BA%8E-' + encodeURIComponent(moment(timestamp).fromNow()) + '-brightgreen.svg');
+    $('#updated-at img').attr('src', 'https://img.shields.io/badge/Updated-' + encodeURIComponent(moment(timestamp).fromNow()) + '-brightgreen.svg');
 };
 var updateAt = $('meta[name=updated_at]').attr('content');
 updateVersion(updateAt);
